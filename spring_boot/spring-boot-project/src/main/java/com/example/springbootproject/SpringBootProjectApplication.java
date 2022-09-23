f7647a018dde6e0899642482a60820e57ba0c322
lh99j <grand2181@gmail.com>
2022-09-23 16:19:43 +0900

    [issue 3] - Add SpringBoot Project

--- spring_boot/spring-boot-project/src/main/java/com/example/springbootproject/SpringBootProjectApplication.java
+++ spring_boot/spring-boot-project/src/main/java/com/example/springbootproject/SpringBootProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProjectApplication.class, args);
     }
